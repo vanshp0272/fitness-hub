11127c8d6a68a0a4892faacc4aeafe84e70761b8
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-02-02 21:08:31 -0600

    Render icon depending on card type

--- src/components/ProgressCard.js
+++ src/components/ProgressCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ProgressCard.css';
-import { IoFootsteps } from 'react-icons/io5';
+import { IoFootsteps, IoWaterSharp } from 'react-icons/io5';
+import { GiNightSleep, GiWeight } from 'react-icons/gi';
 
 export default function ProgressCard({
   name,
@@ -9,11 +10,20 @@ export default function ProgressCard({
   goal,
   units
 }) {
+
+  const getIcon = () => {
+    if (name === 'Walk') return <IoFootsteps className="text text--med" />;
+    if (name === 'Workout') return <GiWeight className="text text--med" />;
+    if (name === "Water") return <IoWaterSharp className="text text--med"/>;
+    if (name === "Sleep") return <GiNightSleep className="text text--med" />;
+    return null;
+  }
+
   return (
     <article className={`card ${cardStyle}`}>
       <div className="card__header">
         <h2 className="text text--med">{name}</h2>
-        <IoFootsteps className="text text--med"/>
+        {getIcon()}
       </div>
       <div className="card__body">
         <p className="text text--small">
