7eff8b314bd69b3934232bf42d45c12456e30a05
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-02-02 20:38:22 -0600

    Design grid layout for card container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import ProgressCard from "./components/ProgressCard";
+import "./App.css";
+
 function App() {
   return (
     <>
-      Hello, World
+      Fitness Hub
+
+      <section className="card-container">
+        <ProgressCard name="First card" />
+        <ProgressCard name="Second card" />
+        <ProgressCard name="Third card" />
+        <ProgressCard name="Fourth card" />
+      </section>
     </>
   );
 }
--- src/components/ProgressCard.js
+++ src/components/ProgressCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './ProgressCard.css';
 
 export default function ProgressCard({ name, count, goal, units }) {
   return (
