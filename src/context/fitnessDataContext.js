2dd540e218e87db7edd02a3ce4a23e53d7c0041f
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-02-05 19:29:20 -0600

    Create functions to get and update fitness data

--- src/context/fitnessDataContext.js
+++ src/context/fitnessDataContext.js
@@ -8,10 +8,37 @@ export function useFitnessData() {
 }
 
 export const FitnessDataProvider = ({ children }) => {
-  // Custom hook to handle local storage items
-  const [fitnessData, setFitnessData] = useLocalStorage("fitnessData", []);
+  // Set fitnessData to local storage value, else to default data
+  const [fitnessData, setFitnessData] = useLocalStorage("fitnessData", [
+    {
+      name: "walk",
+      date: Date.now(),
+      value: 0,
+      goal: 5
+    },
+    {
+      name: "workout",
+      date: Date.now(),
+      value: 0,
+      goal: 2
+    },
+    {
+      name: "water",
+      date: Date.now(),
+      value: 0,
+      goal: 8
+    },
+    {
+      name: "sleep",
+      date: Date.now(),
+      value: 0,
+      goal: 8
+    }
+  ]);
 
   // === FUNCTIONS TO CREATE, READ AND UPDATE FITNESS DATA ===
+
+  // TODO: Reset counters to 0 each day
 
   // Get fitness data based on name, i.e. "walk"
   function getFitnessData(name) {
@@ -19,15 +46,28 @@ export const FitnessDataProvider = ({ children }) => {
   }
 
   // Update count or goal of fitness data with given name
-  function updateFitnessData(name, { count=null, goal=null}) {
+  function updateFitnessData(name, { count, goal }) {
+    // Delete fitness data with given name 
     setFitnessData(previousData => {
-      [...previousData, ]
+      previousData.filter(data => data.name !== name);
+    });
+
+    // Create new data item and append to fitnessData
+    const newData = {
+      name: name,
+      date: Date.now(),
+      count: count,
+      goal: goal
+    };
+    setFitnessData(previousData => {
+      [...previousData, newData]
     });
   }
 
   return <FitnessDataContext.Provider value={{
     fitnessData,
-    getFitnessData
+    getFitnessData,
+    updateFitnessData
   }}>
     {children}
   </FitnessDataContext.Provider>
