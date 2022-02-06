e3b2dd012a763098947516c282cbe4834ba61125
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-02-05 19:30:00 -0600

    Add context to app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { FitnessDataProvider } from './context/fitnessDataContext';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <FitnessDataProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </FitnessDataProvider >,
   document.getElementById('root')
 );
